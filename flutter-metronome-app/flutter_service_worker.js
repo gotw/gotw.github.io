719e2f1ca20397479c07e6849a1b4590844a4e82
Daniel Roberts <deroberts3@gmail.com>
2020-07-20 23:26:05 -0500

    fix safari build

--- flutter-metronome-app/flutter_service_worker.js
+++ flutter-metronome-app/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "3922993ffa3b03ed43ae22e9b4759d0b",
 "/": "3922993ffa3b03ed43ae22e9b4759d0b",
-"main.dart.js": "f2b0f8a54c37503fc4d4bfa22bb008dd",
+"main.dart.js": "25f911a200195c6db41e4bbca5be4889",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
